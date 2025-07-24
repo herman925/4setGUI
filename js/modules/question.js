257e2c9c22bd02b1ac0ac3bea57a2b3552031434
herman925 <hermanchan_925@hotmail.com>
2025-07-24 09:57:11 +0800

    Removed inner html

--- js/modules/question.js
+++ js/modules/question.js
@@ -69,11 +69,7 @@ export function renderCurrentQuestion() {
     // Render based on question type
     switch (question.type) {
         case 'instruction':
-            // Read-only instruction type - no input needed
-            const instructionNote = document.createElement('div');
-            instructionNote.className = 'instruction-note';
-            instructionNote.innerHTML = '<em>請閱讀以上內容後按右下方箭頭（➜）繼續</em>';
-            questionWrapper.appendChild(instructionNote);
+            // Read-only instruction type - no input needed, no additional text
             break;
         case 'text':
             const input = document.createElement('input');
